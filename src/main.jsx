116725f5e9f7c900273d724dac1abc93a3165c98
Gary Yeh <cashbooktw@gmail.com>
2016-06-24 19:52:05 +0800

    4-35 Multiple pages with React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
-var ListManager = require('./components/ListManager.jsx');
+// var ListManager = require('./components/ListManager.jsx');
+var Routes = require('./Routes.jsx');
 
-ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
-ReactDOM.render(<ListManager title="ToDo" />, document.getElementById('todo'));
-ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217"/>, document.getElementById('christmas'));
+// ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
+// ReactDOM.render(<ListManager title="ToDo" />, document.getElementById('todo'));
+// ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217"/>, document.getElementById('christmas'));
+ReactDOM.render(Routes,document.getElementById('main'));
