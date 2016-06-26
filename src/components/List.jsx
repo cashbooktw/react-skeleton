ae3775ed4a4c78455ac43a585386a605bb243d67
Gary Yeh <cashbooktw@gmail.com>
2016-06-26 14:41:51 +0800

    http-fetch

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,26 @@
 var React = require('react');
 var ListItem = require('./ListItem.jsx');
+var HTTP = require('../services/httpservice');
 
 var List = React.createClass({
+  getInitialState: function() {
+    return {ingredients: []};
+  },
+  componentWillMount: function (){
+    HTTP.get('/ingredients').then(function(data) {
+      console.log("DATA: " + data);
+      this.setState({ingredients: data});
+    }.bind(this));
+  },
   render: function(){
     var createItem = function(text, index) {
       return <ListItem key={index + text} text={text}/>;
     };
-
     return (<ul>{this.props.items.map(createItem)}</ul>);
+    // var listItems = this.state.ingredients.map(function(item) {
+    //   return <ListItem key={item.id} ingredient={item.text} />;
+    // });
+    // return (<ul>{listItems}</ul>);
   }
 });
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,5 +3,6 @@ var ReactDOM = require('react-dom');
 var ListManager = require('./components/ListManager.jsx');
 
 ReactDOM.render(<ListManager title="Ingredients" />, document.getElementById('ingredients'));
-ReactDOM.render(<ListManager title="ToDo" />, document.getElementById('todo'));
+// ReactDOM.render(<ListManager title="ToDo" />, document.getElementById('todo'));
 ReactDOM.render(<ListManager title="Christmas" headingColor="#b31217"/>, document.getElementById('christmas'));
+ReactDOM.render(<ListManager title="test" />, document.getElementById('test'));
